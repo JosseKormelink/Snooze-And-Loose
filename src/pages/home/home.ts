17c61e36e52e99be394500ea6bb0d52d925351eb
JosseKormelink <jossekormelink@hotmail.com>
2017-08-01 20:20:55 +0200

    setup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private facebookConnectPlugin: Facebook) {
 
+  }
+
+  postRandomPic() {
+    this.facebookConnectPlugin
   }
 
 }
